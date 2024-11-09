81e91bae1f3698946b3a9b0f94a444e43c022825
Prowindows62 <prowindows62@gmail.com>
2024-11-09 22:40:55 +0300

    Initial commit

---
+++ LockPC/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace LockPC
+{
+    class Program
+    {
+        [DllImport("user32.dll")]
+        private static extern bool LockWorkStation();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Locking the workstation...");
+            LockWorkStation();
+        }
+    }
+}
